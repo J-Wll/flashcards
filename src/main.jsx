607a67287a8ab972eaf6ccfef5657d7ec99907f8
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-22 15:51:16 +0100

    vite, react and bootstrap config

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <p className="mb-0">dds</p>
+    <p>dds</p>
+    <p>dds</p>
+
+  </React.StrictMode>,
+)
