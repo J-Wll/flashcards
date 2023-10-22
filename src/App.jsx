7ab0b8537eaa51659841baa4ab06a7469a5fe99a
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-22 16:48:07 +0100

    Basic div placement for testing

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <>
-      <div className="flashCard"></div>
+      <div className='flashcard-container box-border mx-auto h-screen dark:bg-slate-800 bg-slate-100 flex align-middle items-center justify-center '>
+        <div className="box-border flashcard min-w-6 w-[600px] h-2/3 rounded-3xl bg-black  ">
+        </div>
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App className=""/>
-    <p className="mb-20 text-cyan-800">dds</p>
-    <p>dds</p>
-    <p>dds</p>
+    
+    <App/>
 
   </React.StrictMode>,
 )
