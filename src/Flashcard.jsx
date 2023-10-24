414c711875f3c5489754ebaab7c99292e21d39c8
Joe-W <80954812+J-Wll@users.noreply.github.com>
2023-10-24 15:35:42 +0100

    Added content and formatting within the flashcards

--- src/Flashcard.jsx
+++ src/Flashcard.jsx
@@ -5,9 +5,19 @@ function conditionalReturn(props) {
     if (props.inactive) {
         return (<button onClick={props.onClick} className={`flashcard inactive`}></button>)
     }
-    else { return (<div id="active-flashcard" className={`flashcard active ${props.extraClasses}`}>
-        <p className="text-white">This is a sample question?</p>
-    </div>) }
+    else {
+        return (<div id="active-flashcard" className={`flashcard active ${props.extraClasses}`}>
+            <p className="text-white">This is a sample question?</p>
+            <div className="divider"></div>
+            <p className="text-white">Answer here</p>
+            <ol className="text-white">
+                <li>Multiple choice 1</li>
+                <li>Multiple choice 2</li>
+                <li>Multiple choice 3</li>
+                <li>Multiple choice 4</li>
+            </ol>
+        </div>)
+    }
 }
 
 export default function Flashcard(props) {
