acad3e39ed17c0a4dc36638d3962dbf66fa5632f
Joe-W <80954812+J-Wll@users.noreply.github.com>
2024-01-12 17:09:10 +0000

    Styling changes
    Use of CSS variables
    Added a large font size for 1440p+
    Removed flex-container, placing all the styling onto root
    General cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import './css/Utility.css'
 function App() {
 
   return (
-    <div className="flex-container">
+    <>
       <h1 className='text-white ft-5 app-title'>Flashcards</h1>
       <FlashcardHandler />
-    </div>
+    </>
   )
 }
 
